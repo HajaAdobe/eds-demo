ef1f0155dff345095e395592e481a8e48645faaa
O M Haja Nivas <oh001025543@techmahindra.com>
2024-08-21 14:42:50 +0530

    build issue fix

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,7 +1,6 @@
 import { createOptimizedPicture, fetchPlaceholders } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-
   const placeholders = await fetchPlaceholders('');
   const { clickHereForMore } = placeholders;
 
@@ -9,20 +8,29 @@ export default async function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    const div = document.createElement('div');
+    const divWrapper = document.createElement('div');
     const url = '/home';
-    const footer = `<a href='${url}'>${clickHereForMore}</a>`
-    div.classList.add('cards-card-footer');
-    div.innerHTML = footer;
+    const footer = `<a href='${url}'>${clickHereForMore}</a>`;
+    divWrapper.classList.add('cards-card-footer');
+    divWrapper.innerHTML = footer;
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+    [...li.children].forEach((childDiv) => {
+      if (childDiv.children.length === 1 && childDiv.querySelector('picture')) {
+        childDiv.className = 'cards-card-image';
+      } else {
+        childDiv.className = 'cards-card-body';
+      }
     });
     ul.append(li);
-    li.append(div);
+    li.append(divWrapper);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('picture > img').forEach((img) =>
+    img
+      .closest('picture')
+      .replaceWith(
+        createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])
+      )
+  );
   block.textContent = '';
   block.append(ul);
 }
